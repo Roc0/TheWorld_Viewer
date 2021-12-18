0d5855616b637f388cd45eb0d5cd496a372c6050
Riccardo Marini <riccardo.marini65@gmail.com>
2021-12-18 13:00:40 +0100

    wip

--- godot_proj/init/Globals.gd
+++ godot_proj/init/Globals.gd
@@ -1,13 +1,23 @@
 extends Node
 
 var debug_enabled : bool = true
+var GDNTheWorldViewer = preload("res://native/GDN_TheWorld_Viewer.gdns").new()
 
-#var player
-
-const QUAD_SIZE := 2								# coordinate spaziali
-const CHUNK_QUAD_COUNT := 50
-const CHUNK_SIZE = QUAD_SIZE * CHUNK_QUAD_COUNT		# coordinate spaziali
+#const QUAD_SIZE := 2								# coordinate spaziali
+#const CHUNK_QUAD_COUNT := 50
+#const CHUNK_SIZE = QUAD_SIZE * CHUNK_QUAD_COUNT		# coordinate spaziali
 
 func _ready():
-	pass
-	#player = get_tree().get_root().find_node("Player", true, false)
+	var _result = connect("tree_exiting", self, "exitFunct",[])
+	GDNTheWorldViewer.set_debug_enabled(Globals.debug_enabled)
+	GDNTheWorldViewer.debug_print("Debug Enabled!")
+	GDNTheWorldViewer.debug_print( "Chunk size = " + str(GDNTheWorldViewer.globals().chunk_size()))
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func exitFunct():
+	GDNTheWorldViewer.debug_print(self.name + ": exitFunct!")
+	GDNTheWorldViewer.destroy()
+	GDNTheWorldViewer.free()
