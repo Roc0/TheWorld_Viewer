4404d47cb5231386f238fa6ad925c73b7d97dff4
Riccardo Marini <riccardo.marini65@gmail.com>
2022-01-30 18:28:39 +0100

    wip

--- godot_proj/init/Globals.gd
+++ godot_proj/init/Globals.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var debug_enabled : bool = true
-var GDNTheWorldViewer = preload("res://native/GDN_TheWorld_Viewer.gdns").new()
+var GDNTheWorldMain = preload("res://native/GDN_TheWorld_Viewer.gdns").new()
 
 var num_vertices_per_chunk_side : int
 var bitmap_resolution : int
@@ -15,45 +15,54 @@ var _result : bool
 #const CHUNK_SIZE = QUAD_SIZE * CHUNK_QUAD_COUNT		# coordinate spaziali
 
 func _ready():
+	var w = get_tree().get_root().find_node("TheWorld_MainNode", true, false)
+	GDN_main().init(w)
+	num_vertices_per_chunk_side = GDN_globals().get_num_vertices_per_chunk_side()
+	
 	#var _result = connect("tree_exiting", self, "exitFunct",[])
 	assert(connect("tree_exiting", self, "exitFunct",[]) == 0)
-	GDNTheWorldViewer.set_debug_enabled(Globals.debug_enabled)
+	GDN_globals().set_debug_enabled(Globals.debug_enabled)
 	debug_print("Debug Enabled!")
 	
-	bitmap_resolution = GDNTheWorldViewer.globals().get_bitmap_resolution()
+	bitmap_resolution = GDN_globals().get_bitmap_resolution()
 	debug_print("Bitmap resolution = " + str(bitmap_resolution) + " + 1")
-	num_vertices_per_chunk_side = GDNTheWorldViewer.globals().get_num_vertices_per_chunk_side()
+	num_vertices_per_chunk_side = GDN_globals().get_num_vertices_per_chunk_side()
 	debug_print("Num vertices per chunk side = " + str(num_vertices_per_chunk_side) + " + 1")
-	lod_max_depth = GDNTheWorldViewer.globals().get_lod_max_depth()
-	num_lods = GDNTheWorldViewer.globals().get_num_lods()
+	lod_max_depth = GDN_globals().get_lod_max_depth()
+	num_lods = GDN_globals().get_num_lods()
 	debug_print("Lod max depth = " + str(lod_max_depth) + " - Num lods = " + str(num_lods))
 	
 	for lod in range(num_lods):
 		var chunks := get_chunks_per_bitmap_side(lod)
-		var grid_step : float = GDNTheWorldViewer.globals().get_grid_step_in_wu(lod)
+		var grid_step : float = GDN_globals().get_grid_step_in_wu(lod)
 		var verticesPerSide = chunks * num_vertices_per_chunk_side
-		GDNTheWorldViewer.debug_print("lod (" + str(lod) + ") - Num chunks per bitmap side = " + str(chunks) + " - Grid step in WUs = " + str(grid_step) + " - Num vertices per side = " + str(verticesPerSide) + " + 1 - Size of the side in WUs = " + str(verticesPerSide * grid_step))
+		debug_print("lod (" + str(lod) + ") - Num chunks per bitmap side = " + str(chunks) + " - Grid step in WUs = " + str(grid_step) + " - Num vertices per side = " + str(verticesPerSide) + " + 1 - Size of the side in WUs = " + str(verticesPerSide * grid_step))
 
-	var w = get_tree().get_root().find_node("World", true, false)
-	#_result = GDNTheWorldViewer.init(w, 1195425.176295 + 100, 5465512.560295 +100, 0)
-	_result = GDNTheWorldViewer.init(w, 1195425.176295, 5465512.560295, 0)
+	GDN_viewer().set_initial_world_viewer_pos(1195425.176295 + 100, 5465512.560295 +100, 0)
+	#_result = GDN_viewer().set_initial_world_viewer_pos(1195425.176295, 5465512.560295, 0)
 	
-func GDN():
-	return GDNTheWorldViewer
+func GDN_main():
+	return GDNTheWorldMain
+
+func GDN_globals():
+	return GDN_main().globals(true)
+
+func GDN_viewer():
+	return GDN_globals().viewer(true)
 
 func debug_print(var text : String):
-	GDNTheWorldViewer.debug_print(text)
+	GDN_globals().debug_print(text)
 
 func get_chunks_per_bitmap_side(var lod : int) -> int:
-	return GDNTheWorldViewer.globals().get_chunks_per_bitmap_side(lod)
+	return GDN_globals().get_chunks_per_bitmap_side(lod)
 
 func get_grid_step_in_wu(var lod : int) -> float:
-	return GDNTheWorldViewer.globals().get_grid_step_in_wu(lod)
+	return GDN_globals().get_grid_step_in_wu(lod)
 
 func exitFunct():
-	GDNTheWorldViewer.debug_print(self.name + ": exitFunct!")
-	GDNTheWorldViewer.destroy()
-	GDNTheWorldViewer.free()
+	debug_print(self.name + ": exitFunct!")
+	#GDN_main.destroy()
+	#GDN_main().free()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
