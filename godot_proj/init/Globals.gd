50b4fda8e6e87bf3b2d1162d8d7053f4fe2a10c8
Riccardo Marini <riccardo.marini65@gmail.com>
2022-03-05 21:59:03 +0100

    wip

--- godot_proj/init/Globals.gd
+++ godot_proj/init/Globals.gd
@@ -2,53 +2,52 @@ extends Node
 
 var debug_enabled : bool = true
 var GDNTheWorldMain = preload("res://native/GDN_TheWorld_Viewer.gdns").new()
+var GDNTheWorldGlobals : Node = null
 
 var num_vertices_per_chunk_side : int
 var bitmap_resolution : int
 var lod_max_depth : int
 var num_lods : int
 var _result : bool
-var world_viewer_initialized : bool = false
-
-var initialViewerPos := Vector3(1195425.176295 + 100, 0, 5465512.560295 +100)
-var initiaCameraDistanceFromTerrain = 20
-var initialLevel := 0
-
-var sun : DirectionalLight = null
+var world_initialized : bool = false
+var main_node : Node
+var world_main_node : Spatial
 
 func _ready():
 	assert(connect("tree_exiting", self, "exit_funct",[]) == 0)
+	OS.set_window_maximized(true)
 	
-	sun = get_tree().get_root().find_node("Sun", true, false)
-	
-	var m = get_tree().get_root().find_node("Main", true, false)
-	var w = get_tree().get_root().find_node("TheWorld_Main", true, false)
-	GDN_main().init(m, w)
+	main_node = get_tree().get_root().find_node("Main", true, false)
+	world_main_node = get_tree().get_root().find_node("TheWorld_Main", true, false)
+	GDN_main().init(main_node, world_main_node)
 
 	GDN_globals().set_debug_enabled(Globals.debug_enabled)
 	debug_print("Debug Enabled!")
 
-	num_vertices_per_chunk_side = GDN_globals().get_num_vertices_per_chunk_side()
+	printTerrainDimensions()
 	
-	bitmap_resolution = GDN_globals().get_bitmap_resolution()
-	debug_print("Bitmap resolution = " + str(bitmap_resolution) + " + 1")
-	num_vertices_per_chunk_side = GDN_globals().get_num_vertices_per_chunk_side()
-	debug_print("Num vertices per chunk side = " + str(num_vertices_per_chunk_side) + " + 1")
-	lod_max_depth = GDN_globals().get_lod_max_depth()
-	num_lods = GDN_globals().get_num_lods()
-	debug_print("Lod max depth = " + str(lod_max_depth) + " - Num lods = " + str(num_lods))
+func _notification(what):
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if not world_initialized:
+		initialize_world()
+		world_initialized = true
+
+func initialize_world() -> void:
+	world_main_node.enter_world()
 	
-	for lod in range(num_lods):
-		var chunks := get_chunks_per_bitmap_side(lod)
-		var grid_step : float = GDN_globals().get_grid_step_in_wu(lod)
-		var verticesPerSide = chunks * num_vertices_per_chunk_side
-		debug_print("lod (" + str(lod) + ") - Num chunks per bitmap side = " + str(chunks) + " - Grid step in WUs = " + str(grid_step) + " - Num vertices per side = " + str(verticesPerSide) + " + 1 - Size of the side in WUs = " + str(verticesPerSide * grid_step))
+func unitialize_world() -> void:
+	world_main_node.exit_world()
 
 func GDN_main():
 	return GDNTheWorldMain
 
 func GDN_globals():
-	return GDN_main().globals(true)
+	if GDNTheWorldGlobals == null:
+		GDNTheWorldGlobals = GDN_main().globals(true)
+	return GDNTheWorldGlobals
 
 func GDN_viewer():
 	return GDN_globals().viewer(true)
@@ -63,26 +62,22 @@ func get_grid_step_in_wu(var lod : int) -> float:
 	return GDN_globals().get_grid_step_in_wu(lod)
 
 func exit_funct():
+	debug_print ("Quitting...")
+	unitialize_world()
 	GDN_main().deinit()
-
-
-func _notification(what):
-	if (what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST):
-		quit_app()
-
-func quit_app() -> void:
-	debug_print ("Quitting ...")
-	get_tree().quit()
-
-func intialize_viewer() -> void:
-	GDN_viewer().reset_initial_world_viewer_pos(initialViewerPos.x, initialViewerPos.z, initiaCameraDistanceFromTerrain, initialLevel)
-	OS.window_maximized = true
-	#if sun != null:
-	#	sun.Transform.origin = Vector3(initialViewerPos.x, 100, initialViewerPos.z)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if not world_viewer_initialized:
-		intialize_viewer()
-		world_viewer_initialized = true
-#	pass
+	
+func printTerrainDimensions() -> void:
+	num_vertices_per_chunk_side = GDN_globals().get_num_vertices_per_chunk_side()
+	bitmap_resolution = GDN_globals().get_bitmap_resolution()
+	debug_print("Bitmap resolution = " + str(bitmap_resolution) + " + 1")
+	num_vertices_per_chunk_side = GDN_globals().get_num_vertices_per_chunk_side()
+	debug_print("Num vertices per chunk side = " + str(num_vertices_per_chunk_side) + " + 1")
+	lod_max_depth = GDN_globals().get_lod_max_depth()
+	num_lods = GDN_globals().get_num_lods()
+	debug_print("Lod max depth = " + str(lod_max_depth) + " - Num lods = " + str(num_lods))
+	for lod in range(num_lods):
+		var chunks := get_chunks_per_bitmap_side(lod)
+		var grid_step : float = GDN_globals().get_grid_step_in_wu(lod)
+		var verticesPerSide = chunks * num_vertices_per_chunk_side
+		debug_print("lod (" + str(lod) + ") - Num chunks per bitmap side = " + str(chunks) + " - Grid step in WUs = " + str(grid_step) + " - Num vertices per side = " + str(verticesPerSide) + " + 1 - Size of the side in WUs = " + str(verticesPerSide * grid_step))
+		
---
+++ godot_proj/main/TheWorld_Main.gd
@@ -0,0 +1,79 @@
+extends Spatial
+
+var debug_window_Active : bool = false
+var world_entered : bool = false
+var initialViewerPos := Vector3(1195425.176295 + 100, 0, 5465512.560295 +100)
+var initiaCameraDistanceFromTerrain = 20
+var initialLevel := 0
+var init_world_thread : Thread
+var world_initalized : bool = false
+var request_to_quit_pending : bool = false
+var fps := 0.0
+
+func _ready():
+	pass
+
+func _input(event):
+	if event is InputEventKey:
+		if event.is_action_pressed("ui_toggle_debug_window"):
+			if debug_window_Active:
+				set_debug_window(false)
+			else:
+				set_debug_window(true)
+		elif event.is_action_pressed("ui_cancel"):
+			request_to_quit_pending = true
+				
+func _process(delta):
+	fps = Engine.get_frames_per_second()
+	
+	if init_world_thread.is_alive():
+		return
+	if init_world_thread.is_active():
+		init_world_thread.wait_to_finish()
+	if request_to_quit_pending:
+		force_app_to_quit()
+	if not world_entered:
+		return
+	if not world_initalized:
+		return
+			
+func enter_world():
+	Globals.debug_print("Entering world...")
+	OS.window_maximized = true
+	set_debug_window(false)
+	$DebugStats.add_property(self, "fps", "")
+	world_initalized = false
+	init_world_thread = Thread.new()
+	init_world_thread.start(self, "_init_world")
+	world_entered = true
+	Globals.debug_print("World entered...")
+	
+func exit_world():
+	if world_entered:
+		Globals.debug_print("Exiting world...")
+		$DebugStats.remove_property(self, "fps")
+		if init_world_thread.is_active():
+			init_world_thread.wait_to_finish()
+		world_initalized = false
+		world_entered = false
+		Globals.debug_print("World exited...")
+	
+func set_debug_window(active : bool) -> void:
+	if active:
+		debug_window_Active = true
+		$DebugStats.visible = true
+	else:
+		debug_window_Active = false
+		$DebugStats.visible = false
+		
+func _init_world() -> void:
+	Globals.debug_print("Initializing world...")
+	Globals.GDN_viewer().reset_initial_world_viewer_pos(initialViewerPos.x, initialViewerPos.z, initiaCameraDistanceFromTerrain, initialLevel)
+	Globals.debug_print("World initializatoin completed...")
+	world_initalized = true
+	
+func force_app_to_quit() -> void:
+	get_tree().set_input_as_handled()
+	exit_world()
+	get_tree().quit()
+	
