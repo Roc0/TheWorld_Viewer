9d29a6866bdc5fb0e085f851367a3c51b728a180
Riccardo Marini <riccardo.marini65@gmail.com>
2022-04-09 15:39:46 +0200

    wip

--- godot_proj/main/TheWorld_Main.gd
+++ godot_proj/main/TheWorld_Main.gd
@@ -17,14 +17,16 @@ var num_splits : int
 var num_joins : int
 var num_chunks : int
 var debug_draw_mode : String
-var chunk_debug_mode = -1
-var camera_chunk_id : String = ""
-var camera_chunk_x : String = ""
-var camera_chunk_h : String = ""
-var camera_chunk_z : String = ""
-var camera_chunk_debug_x : String = ""
-var camera_chunk_debug_h : String = ""
-var camera_chunk_debug_z : String = ""
+var chunk_debug_mode  : String = ""
+var cam_chunk_id : String = ""
+var cam_chunk_mesh_pos_xzy : String = ""
+var cam_chunk_mesh_aabb_x : String = ""
+var cam_chunk_mesh_aabb_y : String = ""
+var cam_chunk_mesh_aabb_z : String = ""
+var cam_chunk_dmesh_pos_xzy : String = ""
+var cam_chunk_dmesh_aabb_x : String = ""
+var cam_chunk_dmesh_aabb_y : String = ""
+var cam_chunk_dmesh_aabb_z : String = ""
 		
 func _ready():
 	pass
@@ -62,25 +64,27 @@ func _process(_delta):
 		#cube_mesh.size = Vector3(1, 1, 1)
 		scene_initialized = true
 	
-	var _chunk_debug_mode = viewer.get_chunk_debug_mode()
-	var _camera_chunk_id = viewer.get_camera_chunk_id()
-	if (_camera_chunk_id != camera_chunk_id or _chunk_debug_mode != chunk_debug_mode):
-		#var camera_chunk_t : Transform = Globals.GDN_viewer().get_camera_chunk_global_transform_of_aabb()
-		#var displ : Vector3 = Vector3(camera_chunk_t.basis.x.x, camera_chunk_t.basis.y.y, camera_chunk_t.basis.z.z) / 2
-		#camera_chunk_t.origin += displ
-		#$MeshProva.global_transform = camera_chunk_t
+	var _chunk_debug_mode : String = viewer.get_chunk_debug_mode()
+	var _cam_chunk_id = viewer.get_camera_chunk_id()
+	if (_cam_chunk_id != cam_chunk_id or _chunk_debug_mode != chunk_debug_mode):
+		#var cam_chunk_t : Transform = Globals.GDN_viewer().get_camera_chunk_global_transform_of_aabb()
+		#var displ : Vector3 = Vector3(cam_chunk_t.basis.x.x, cam_chunk_t.basis.y.y, cam_chunk_t.basis.z.z) / 2
+		#cam_chunk_t.origin += displ
+		#$MeshProva.global_transform = cam_chunk_t
 		var t_aabb : AABB = viewer.get_camera_chunk_local_aabb()
 		var t_mesh : Transform = viewer.get_camera_chunk_mesh_global_transform_applied()
-		camera_chunk_x = String(t_mesh.origin.x) + ":" + String(t_mesh.origin.x + t_aabb.size.x)
-		camera_chunk_h = String(t_mesh.origin.y) + ":" + String(t_mesh.origin.y + t_aabb.size.y)
-		camera_chunk_z = String(t_mesh.origin.z) + ":" + String(t_mesh.origin.z + t_aabb.size.z)
+		cam_chunk_mesh_pos_xzy = String(t_mesh.origin.x) + ":" + String(t_mesh.origin.z) + ":" + String(t_mesh.origin.y)
+		cam_chunk_mesh_aabb_x = String(t_mesh.origin.x) + ":" + String(t_mesh.origin.x + t_aabb.size.x)
+		cam_chunk_mesh_aabb_y = String(t_mesh.origin.y) + ":" + String(t_mesh.origin.y + t_aabb.size.y)
+		cam_chunk_mesh_aabb_z = String(t_mesh.origin.z) + ":" + String(t_mesh.origin.z + t_aabb.size.z)
 		t_aabb = viewer.get_camera_chunk_local_debug_aabb()
 		t_mesh = viewer.get_camera_chunk_debug_mesh_global_transform_applied()
-		camera_chunk_debug_x = String(t_mesh.origin.x) + ":" + String(t_mesh.origin.x + t_aabb.size.x)
-		camera_chunk_debug_h = String(t_mesh.origin.y) + ":" + String(t_mesh.origin.y + t_aabb.size.y)
-		camera_chunk_debug_z = String(t_mesh.origin.z) + ":" + String(t_mesh.origin.z + t_aabb.size.z)
+		cam_chunk_dmesh_pos_xzy = String(t_mesh.origin.x) + ":" + String(t_mesh.origin.z) + ":" + String(t_mesh.origin.y)
+		cam_chunk_dmesh_aabb_x = String(t_mesh.origin.x) + ":" + String(t_mesh.origin.x + t_aabb.size.x)
+		cam_chunk_dmesh_aabb_y = String(t_mesh.origin.y) + ":" + String(t_mesh.origin.y + t_aabb.size.y)
+		cam_chunk_dmesh_aabb_z = String(t_mesh.origin.z) + ":" + String(t_mesh.origin.z + t_aabb.size.z)
 		chunk_debug_mode = _chunk_debug_mode
-		camera_chunk_id = _camera_chunk_id
+		cam_chunk_id = _cam_chunk_id
 
 	chunk_grid_global_pos = Globals.GDN_viewer().global_transform.origin
 	if current_camera:
@@ -104,13 +108,16 @@ func enter_world():
 	$DebugStats.add_property(self, "num_chunks", "")
 	$DebugStats.add_property(self, "num_splits", "")
 	$DebugStats.add_property(self, "num_joins", "")
-	$DebugStats.add_property(self, "camera_chunk_id", "")
-	$DebugStats.add_property(self, "camera_chunk_x", "")
-	$DebugStats.add_property(self, "camera_chunk_z", "")
-	$DebugStats.add_property(self, "camera_chunk_h", "")
-	$DebugStats.add_property(self, "camera_chunk_debug_x", "")
-	$DebugStats.add_property(self, "camera_chunk_debug_z", "")
-	$DebugStats.add_property(self, "camera_chunk_debug_h", "")
+	$DebugStats.add_property(self, "cam_chunk_id", "")
+	$DebugStats.add_property(self, "cam_chunk_mesh_pos_xzy", "")
+	$DebugStats.add_property(self, "cam_chunk_mesh_aabb_x", "")
+	$DebugStats.add_property(self, "cam_chunk_mesh_aabb_z", "")
+	$DebugStats.add_property(self, "cam_chunk_mesh_aabb_y", "")
+	$DebugStats.add_property(self, "chunk_debug_mode", "")
+	$DebugStats.add_property(self, "cam_chunk_dmesh_pos_xzy", "")
+	$DebugStats.add_property(self, "cam_chunk_dmesh_aabb_x", "")
+	$DebugStats.add_property(self, "cam_chunk_dmesh_aabb_z", "")
+	$DebugStats.add_property(self, "cam_chunk_dmesh_aabb_y", "")
 	world_initalized = false
 	init_world_thread = Thread.new()
 	var err := init_world_thread.start(self, "_init_world")
@@ -130,13 +137,16 @@ func exit_world():
 		$DebugStats.remove_property(self, "num_chunks")
 		$DebugStats.remove_property(self, "num_splits")
 		$DebugStats.remove_property(self, "num_joins")
-		$DebugStats.remove_property(self, "camera_chunk_id")
-		$DebugStats.remove_property(self, "camera_chunk_x")
-		$DebugStats.remove_property(self, "camera_chunk_z")
-		$DebugStats.remove_property(self, "camera_chunk_h")
-		$DebugStats.remove_property(self, "camera_chunk_debug_x")
-		$DebugStats.remove_property(self, "camera_chunk_debug_z")
-		$DebugStats.remove_property(self, "camera_chunk_debug_h")
+		$DebugStats.remove_property(self, "cam_chunk_id")
+		$DebugStats.remove_property(self, "cam_chunk_mesh_pos_xzy")
+		$DebugStats.remove_property(self, "cam_chunk_mesh_aabb_x")
+		$DebugStats.remove_property(self, "cam_chunk_mesh_aabb_z")
+		$DebugStats.remove_property(self, "cam_chunk_mesh_aabb_y")
+		$DebugStats.remove_property(self, "chunk_debug_mode")
+		$DebugStats.remove_property(self, "cam_chunk_dmesh_pos_xzy")
+		$DebugStats.remove_property(self, "cam_chunk_dmesh_aabb_x")
+		$DebugStats.remove_property(self, "cam_chunk_dmesh_aabb_z")
+		$DebugStats.remove_property(self, "cam_chunk_dmesh_aabb_y")
 		if init_world_thread.is_active():
 			init_world_thread.wait_to_finish()
 		world_initalized = false
