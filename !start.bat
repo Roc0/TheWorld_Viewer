5611f8262815c1f9f201aaa10af66a5203a15b49
Riccardo Marini <riccardo.marini65@gmail.com>
2022-06-04 10:47:01 +0200

    wip

--- !start.bat
+++ !start.bat
@@ -5,4 +5,4 @@ cd /D %curpath%
 copy /Y ..\TheWorld_GDN_Viewer\x64\*.dll %curpath%godot_proj\native\
 
 
-call Godot_v3.4.2-stable_win64.exe -v --path %curpath%godot_proj -e
+call Godot_v3.4.2-stable_win64.exe -v --path %curpath%godot_proj -e>log.txt
