b463b3e25133b245c0eb073be4895bf714841f2b
Riccardo Marini <riccardo.marini65@gmail.com>
2022-08-14 12:33:14 +0200

    aa

--- !start.bat
+++ !start.bat
@@ -3,6 +3,7 @@ set curpath=%~dp0
 cd /D %curpath%
 
 copy /Y ..\TheWorld_GDN_Viewer\x64\*.dll %curpath%godot_proj\native\
+copy /Y ..\shapelib\shapelib.dll %curpath%godot_proj\native\
 
 
 call Godot_v3.4.2-stable_win64.exe -v --path %curpath%godot_proj -e>log.txt
