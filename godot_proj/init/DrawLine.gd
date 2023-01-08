26744b558e7e56639f8103847ca906c364ca2e02
Riccardo Marini <riccardo.marini65@gmail.com>
2023-01-08 20:27:11 +0100

    wip

--- godot_proj/init/DrawLine.gd
+++ godot_proj/init/DrawLine.gd
@@ -17,7 +17,7 @@ func _draw():
 	for line in Lines:
 		draw_line(line.a, line.b, line.color, line.thickness)
 
-func _process(delta):
+func _process(_delta):
 	update()
 
 func Draw_Line3D(id : int, vector_a : Vector3, vector_b : Vector3, color : Color, thickness : float):
@@ -25,16 +25,20 @@ func Draw_Line3D(id : int, vector_a : Vector3, vector_b : Vector3, color : Color
 	for line in Lines:
 		if line.id == id:
 			line.color = color
-			line.a = Camera_Node.unproject_position(vector_a)
-			line.b = Camera_Node.unproject_position(vector_b)
+			var start : Vector2 = Camera_Node.unproject_position(vector_a)
+			var end : Vector2 = Camera_Node.unproject_position(vector_b)
+			line.a = start
+			line.b = end
 			line.thickness = thickness
 			return
 
 	var new_line = Line.new()
 	new_line.id = id
 	new_line.color = color
-	new_line.a = Camera_Node.unproject_position(vector_a)
-	new_line.b = Camera_Node.unproject_position(vector_b)
+	var start : Vector2 = Camera_Node.unproject_position(vector_a)
+	var end : Vector2 = Camera_Node.unproject_position(vector_b)
+	new_line.a = start
+	new_line.b = end
 	new_line.thickness = thickness
 	Lines.append(new_line)
 
--- godot_proj/init/Globals.gd
+++ godot_proj/init/Globals.gd
@@ -10,8 +10,8 @@ const clientstatus_connected_to_server = 2
 const clientstatus_str_connected_to_server = "connected_to_server"
 const clientstatus_session_initialized = 3
 const clientstatus_str_session_initialized = "session_initialized"
-const clientstatus_world_deply_in_progress = 4
-const clientstatus_str_world_deply_in_progress = "world_deply_in_progress"
+const clientstatus_world_deploy_in_progress = 4
+const clientstatus_str_world_deploy_in_progress = "world_deploy_in_progress"
 const clientstatus_world_deployed = 5
 const clientstatus_str_world_deployed = "world_deployed"
 
@@ -138,8 +138,8 @@ func status_to_string(var status : int) -> String:
 		return clientstatus_str_connected_to_server
 	elif status == clientstatus_session_initialized:
 		return clientstatus_str_session_initialized
-	elif status == clientstatus_world_deply_in_progress:
-		return clientstatus_str_world_deply_in_progress
+	elif status == clientstatus_world_deploy_in_progress:
+		return clientstatus_str_world_deploy_in_progress
 	elif status == clientstatus_world_deployed:
 		return clientstatus_str_world_deployed
 	else:
