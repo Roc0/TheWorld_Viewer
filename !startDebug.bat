cd6c5880569ac40fd439fae9018b1cfb175a0877
Riccardo Marini <riccardo.marini65@gmail.com>
2023-06-13 11:08:18 +0200

    wip

--- !startDebug.bat
+++ !startDebug.bat
@@ -5,4 +5,5 @@ cd /D %curpath%
 copy /Y ..\TheWorld_GDN_Viewer\x64\*.dll %curpath%TheWorld\addons\twviewer\native\
 copy /Y ..\shapelib\shapelib.dll %curpath%TheWorld\addons\twviewer\native\
 
-call godot.windows.editor.dev.x86_64.console.exe -v --path %curpath%TheWorld -e>log.txt
+call ..\..\Prove\godot\bin\godot.windows.editor.dev.x86_64.console.exe -v --path %curpath%TheWorld -e>log.txt
+
