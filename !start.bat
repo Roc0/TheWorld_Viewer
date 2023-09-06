08a8612d0780f82d8722a21ac62b235847150305
Riccardo Marini <riccardo.marini65@gmail.com>
2023-09-06 20:52:51 +0200

    wip

--- !start.bat
+++ !start.bat
@@ -5,4 +5,4 @@ cd /D %curpath%
 copy /Y ..\TheWorld_GDN_Viewer\x64\*.dll %curpath%TheWorld\addons\twviewer\native\
 copy /Y ..\shapelib\shapelib.dll %curpath%TheWorld\addons\twviewer\native\
 
-call Godot_v4.0.3-stable_win64.exe -v --path %curpath%TheWorld -e>log.txt
+call Godot_v4.1.1-stable_win64.exe -v --path %curpath%TheWorld -e>log.txt
